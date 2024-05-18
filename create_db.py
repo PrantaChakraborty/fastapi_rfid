f64b74e6f37db847de5e8f0b808c8e655a28794a
PrantaChakraborty <cbpranta@gmail.com>
2024-05-18 18:15:05 +0600

    remove create_db.py

--- create_db.py
+++
@@ -1,8 +0,0 @@
-from src.database import engine
-from src.auth import models
-
-print("creating db ...")
-try:
-    models.Base.metadata.create_all(bind = engine)
-except Exception as e:
-    print(e)
