2e1a1d7f279345900b092d9bc3a54cdb908835d7
PrantaChakraborty <cbpranta@gmail.com>
2024-05-18 17:01:57 +0600

    Update methods

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,17 +1,13 @@
-from datetime import datetime, timedelta
+import datetime
 from typing import Union, Any, Optional
 
 from fastapi.security import OAuth2PasswordBearer
-from fastapi import HTTPException, Request
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from passlib.context import CryptContext
 from jose import jwt
 
 from .models import *
 from src.config import setting
-from .exceptions import InvalidTokenError
-
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -26,12 +22,9 @@ def verify_pwd(plain, hash_passwd):
     return pwd_context.verify(plain, hash_passwd)
 
 
-def create_access_token(subject: Union[str, Any],
-                        expires_delta: int = None) -> str:
-    if expires_delta is not None:
-        expires_delta = datetime.utcnow() + expires_delta
-    else:
-        expires_delta = datetime.utcnow() + timedelta(
+def create_access_token(subject: Union[str, Any]) -> str:
+
+    expires_delta = datetime.datetime.utcnow() + datetime.timedelta(
             minutes=setting.ACCESS_TOKEN_EXPIRE_MINUTES)
 
     to_encode = {"exp": expires_delta, "sub": str(subject)}
@@ -39,12 +32,9 @@ def create_access_token(subject: Union[str, Any],
     return encoded_jwt
 
 
-def create_refresh_token(subject: Union[str, Any],
-                         expires_delta: int = None) -> str:
-    if expires_delta is not None:
-        expires_delta = datetime.utcnow() + expires_delta
-    else:
-        expires_delta = datetime.utcnow() + timedelta(
+def create_refresh_token(subject: Union[str, Any]) -> str:
+
+    expires_delta = datetime.datetime.utcnow() + datetime.timedelta(
             minutes=setting.REFRESH_TOKEN_EXPIRE_MINUTES)
 
     to_encode = {"exp": expires_delta, "sub": str(subject)}
@@ -52,45 +42,3 @@ def create_refresh_token(subject: Union[str, Any],
                              setting.algorithm)
     return encoded_jwt
 
-
-def decode_jwt(jwtoken: str):
-    try:
-        payload = jwt.decode(jwtoken, setting.secret_key, setting.algorithm)
-        return payload
-    except InvalidTokenError:
-        return None
-
-
-class JWTBearer(HTTPBearer):
-    def __init__(self, auto_error: bool = True):
-        super(JWTBearer, self).__init__(auto_error=auto_error)
-
-    async def __call__(self, request: Request) -> Optional[str]:
-        credentials: HTTPAuthorizationCredentials = await super(JWTBearer,
-                                                                self).__call__(
-            request)
-        if credentials:
-            if not credentials.scheme == "Bearer":
-                raise HTTPException(status_code=403,
-                                    detail="Invalid authentication scheme.")
-            token = credentials.credentials
-            if not self.verify_jwt(token):
-                raise HTTPException(status_code=403,
-                                    detail="Invalid token or expired token.")
-            return token
-        else:
-            raise HTTPException(status_code=403,
-                                detail="Invalid authorization code.")
-
-    def verify_jwt(self, jwtoken: str) -> bool:
-        try:
-            payload = decode_jwt(jwtoken)
-            if payload is not None:
-                return True
-            return False
-        except jwt.ExpiredSignatureError:
-            return False
-        except jwt.JWTError:
-            return False
-
-# jwt_bearer = JWTBearer()
