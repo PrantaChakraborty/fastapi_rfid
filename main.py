3bc4f52879422f6c5a896c416a3f50fd6397b4cd
PrantaChakraborty <cbpranta@gmail.com>
2024-05-19 16:42:14 +0600

    Add CORS origin

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.auth import router
 
 # app = FastAPI(dependencies=[Depends(get_query_token)])
 app = FastAPI()
+origins = [
+    "http://localhost:8080",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router.auth_route)
 # app.include_router(items.router)
