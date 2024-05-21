82a1c2b5063567c10afd4ff4b0d3a3d7dff9cf67
PrantaChakraborty <cbpranta@gmail.com>
2024-05-21 13:41:50 +0600

    update pool size

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ SQL_ALCHEMY_DB_URL = (f"postgresql://{setting.db_usr}:"
                       f"{setting.db_pwd}@{setting.db_host}:"
                       f"{setting.db_port}/{setting.db_name}")
 
-engine = create_engine(SQL_ALCHEMY_DB_URL)
+engine = create_engine(SQL_ALCHEMY_DB_URL, pool_size=100, max_overflow=200)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
